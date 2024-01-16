dfe76177102a379f228b6379ec7cdaa086805eef
RajanBakshi238 <rajan.bakshi238@gmail.com>
2024-01-17 01:25:07 +0530

    interceptor replaced with middleware

---
+++ src/users/middlewares/current-middleware.ts
@@ -0,0 +1,24 @@
+import { Injectable, NestMiddleware } from "@nestjs/common";
+import { Request, Response, NextFunction} from "express";
+import { UsersService } from "../users.service";
+
+@Injectable()
+export class CurrentUserMiddleware implements NestMiddleware{
+    
+    constructor(
+        private usersService: UsersService
+    ){}
+    
+    // to define a middleware we must define a async function
+    async use(req: Request,res: Response, next: NextFunction){
+        const {userId} = req.session || {};
+
+        if(userId){
+            const user = await this.usersService.findOne(userId)
+            // @ts-ignore
+            req.currentUser = user;
+        } 
+
+        next()
+    }
+}
